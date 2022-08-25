337dc57d74f153aaa7f58b802060a7c737843649
Vitor Lira <vitorlirasti@hotmail.com>
2022-08-25 19:58:05 -0300

    mudanças no servidor

--- server.js
+++ server.js
@@ -4,14 +4,16 @@ const conexao = require('./conexao')
 app.use(express.json())
 const port = process.env.PORT || 5000
 const userRoute = require('./routes/userRoute')
-const path = require('path')
+const path = require("path");
 app.use('/api/user', userRoute)
 
-if(process.env.MODE_ENV === 'production'){
-    app.use('/', express.static('client/build'))
-    app.get('*',(req, res)=>{
-        res.sendFile(path.resolve(__dirname , 'client/build/index.html'))
-    })
-}
+if (process.env.NODE_ENV === "production") {
+    app.use("/", express.static("client/build"));
+  
+    app.get("*", (req, res) => {
+      res.sendFile(path.resolve(__dirname, "client/build/index.html"));
+    });
+  }
+
 app.get('/',(req, res) => res.send(Hello))
 app.listen(port, () => console.log(`port ${port}` ))
